b1299bdec673bc5502eef8bcdcdf798e4f7e2671
Cédric Couche <cedric.couche@protonmail.com>
2023-05-29 14:46:54 +0000

    update api

--- container_FastAPI/api_moviereco.py
+++ container_FastAPI/api_moviereco.py
@@ -82,7 +82,7 @@ class Movie(BaseModel):
 api = FastAPI(
     title="Movie recommendation",
     description="Content based Movie recommendation",
-    version="1.3.1",
+    version="1.3.0",
     openapi_tags=[
               {'name':'Info', 'description':'Info'},
               {'name':'MovieReco','description':'Get recommendation'}, 
